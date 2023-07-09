b97396b1689885abcff7b9566fb773be6ca6b290
arovai <antonin.rovai@erasme.ulb.ac.be>
2023-07-09 22:15:02 +0200

    added fsl profile info

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+
+# FSL Setup
+FSLDIR=/opt/fsl
+PATH=${FSLDIR}/share/fsl/bin:${PATH}
+export FSLDIR PATH
+. ${FSLDIR}/etc/fslconf/fsl.sh
+
 # launch cvrmap with arguments
 /opt/cvrmap/cvrmap/cvrmap "$@"
