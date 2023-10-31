56e71f5c82a48fd9d415e463ea0e0ed41be12738
arovai <antonin.rovai@erasme.ulb.ac.be>
2023-10-31 10:34:26 +0100

    create pipy setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CVRmap',
+    version='1.1.4.0',
+    url='https://github.com/ln2t/CVRmap',
+    author='Antonin Rovai',
+    author_email='antonin.rovai@hubruxelles.be',
+    description='CVRmap is an opensource software to compute maps of Cerebro-Vascular Reactivity',
+    packages=find_packages(),
+    install_requires=[],
+)
