94ba3b0d4889ce5c9449d4ef8cb4a42c450c7bd0
arovai <antonin.rovai@erasme.ulb.ac.be>
2023-11-04 21:39:46 +0100

    removed fsl related things

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# FSL Setup
-FSLDIR=/opt/fsl
-PATH=${FSLDIR}/share/fsl/bin:${PATH}
-export FSLDIR PATH
-. ${FSLDIR}/etc/fslconf/fsl.sh
-
 # launch cvrmap with arguments
 /opt/cvrmap/cvrmap/cvrmap.py "$@"
