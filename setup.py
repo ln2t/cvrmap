fad4a91133bd2c255bac136f8266f46aeeaf941e
arovai <antonin.rovai@erasme.ulb.ac.be>
2023-11-24 10:27:07 +0100

    added install all requirements when installing pip and add cli

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
+# Function to read the contents of the requirements file
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
 setup(
-    name='CVRmap',
-    version='1.1.4.0',
-    url='https://github.com/ln2t/CVRmap',
+    name='cvrmap',
+    version='2.0.7',
+    url='https://github.com/ln2t/cvrmap',
     author='Antonin Rovai',
     author_email='antonin.rovai@hubruxelles.be',
     description='CVRmap is an opensource software to compute maps of Cerebro-Vascular Reactivity',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=read_requirements(),
+    entry_points={
+        'console_scripts': [
+            'cvrmap = cvrmap.cvrmap:main',
+        ]}
+
 )
